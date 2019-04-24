78cfb6a213bad69903fc46a83148d41d0b8180d7
Allen Lin <cheetah0513@gmail.com>
2019-04-23 23:24:51 -0500

    Remove the debug code

--- src/main/java/ProcessBatchImageRunnable.java
+++ src/main/java/ProcessBatchImageRunnable.java
@@ -504,8 +504,6 @@ public class ProcessBatchImageRunnable implements Runnable {
                 // If the orginal text has been cached
                 if (translationCachedResult != null) {
                     englishText = translationCachedResult;
-                    System.out.println("Got translation from cache!");
-                    System.out.println(strip_original + " is translated to " + translationCachedResult);
                 } else {
                     // If not cached, use the translation api
                     englishText = translateAPI.translate(strip_original, lang, "en");
